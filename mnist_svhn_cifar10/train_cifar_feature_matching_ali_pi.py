929688660b6e445c1e27cb35a73b734566f487a8
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2017-03-28 15:56:49 -0700

    ali

--- mnist_svhn_cifar10/train_cifar_feature_matching_ali_pi.py
+++ mnist_svhn_cifar10/train_cifar_feature_matching_ali_pi.py
@@ -53,32 +53,63 @@ testx = whitener.apply(testx)
 nr_batches_train = int(trainx.shape[0]/args.batch_size)
 nr_batches_test = int(testx.shape[0]/args.batch_size)
 
-# specify generative model
+# input layers
 noise_dim = (args.batch_size, 100)
 noise = theano_rng.uniform(size=noise_dim)
-gen_layers = [ll.InputLayer(shape=noise_dim, input_var=noise)]
-gen_layers.append(nn.batch_norm(ll.DenseLayer(gen_layers[-1], num_units=4*4*512, W=Normal(0.05), nonlinearity=nn.relu), g=None))
+x_input = ll.InputLayer(shape=(None, 3, 32, 32))
+z_input = ll.InputLayer(shape=noise_dim, input_var=noise)
+
+# specify generative model
+gen_layers = [z_input]
+gen_layers.append(nn.batch_norm(ll.DenseLayer(gen_layers[-1], num_units=4*4*512, W=Normal(0.05), nonlinearity=nn.relu, name='g1'), g=None))
 gen_layers.append(ll.ReshapeLayer(gen_layers[-1], (args.batch_size,512,4,4)))
-gen_layers.append(nn.batch_norm(nn.Deconv2DLayer(gen_layers[-1], (args.batch_size,256,8,8), (5,5), W=Normal(0.05), nonlinearity=nn.relu), g=None)) # 4 -> 8
-gen_layers.append(nn.batch_norm(nn.Deconv2DLayer(gen_layers[-1], (args.batch_size,128,16,16), (5,5), W=Normal(0.05), nonlinearity=nn.relu), g=None)) # 8 -> 16
-gen_layers.append(nn.weight_norm(nn.Deconv2DLayer(gen_layers[-1], (args.batch_size,3,32,32), (5,5), W=Normal(0.05), nonlinearity=T.tanh), train_g=True, init_stdv=0.1)) # 16 -> 32
+gen_layers.append(nn.batch_norm(nn.Deconv2DLayer(gen_layers[-1], (args.batch_size,256,8,8), (5,5), W=Normal(0.05), nonlinearity=nn.relu, name='g2'), g=None)) # 4 -> 8
+gen_layers.append(nn.batch_norm(nn.Deconv2DLayer(gen_layers[-1], (args.batch_size,128,16,16), (5,5), W=Normal(0.05), nonlinearity=nn.relu, name='g3'), g=None)) # 8 -> 16
+gen_layers.append(nn.weight_norm(nn.Deconv2DLayer(gen_layers[-1], (args.batch_size,3,32,32), (5,5), W=Normal(0.05), nonlinearity=T.tanh, name='g4'), train_g=True, init_stdv=0.1)) # 16 -> 32
 gen_dat = ll.get_output(gen_layers[-1])
 
+genz_layers = [x_input]
+genz_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(genz_layers[-1], 64, (3,3), pad=1, stride=2, W=Normal(0.05), nonlinearity=nn.lrelu, name='gz1')))
+genz_layers.append(ll.DropoutLayer(genz_layers[-1], p=0.5))
+genz_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(genz_layers[-1], 128, (3,3), pad=1, stride=2, W=Normal(0.05), nonlinearity=nn.lrelu, name='gz2')))
+genz_layers.append(ll.DropoutLayer(genz_layers[-1], p=0.5))
+# genz_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(genz_layers[-1], 256, (3,3), pad=0, W=Normal(0.05), nonlinearity=nn.lrelu, name='gz3')))
+genz_layers.append(nn.weight_norm(ll.NINLayer(genz_layers[-1], num_units=128, W=Normal(0.05), nonlinearity=nn.lrelu, name='gz5')))
+genz_layers.append(ll.GlobalPoolLayer(genz_layers[-1], name='gz6'))
+genz_layers.append(nn.weight_norm(ll.DenseLayer(genz_layers[-1], num_units=100, W=Normal(0.05), nonlinearity=lasagne.nonlinearities.tanh, name='gz7'), train_g=True, init_stdv=0.1))
+genz_layers.append(ll.ReshapeLayer(genz_layers[-1], noise_dim))
+
+
 # specify discriminative model
-disc_layers = [ll.InputLayer(shape=(None, 3, 32, 32))]
+
+# for z
+discz_layers = [z_input]
+discz_layers.append(ll.DropoutLayer(discz_layers[-1], p=0.2))
+discz_layers.append(nn.weight_norm(ll.DenseLayer(discz_layers[-1], 128, W=Normal(0.05), nonlinearity=nn.lrelu, name='dz1')))
+discz_layers.append(ll.DropoutLayer(discz_layers[-1], p=0.5))
+discz_layers.append(nn.weight_norm(ll.DenseLayer(discz_layers[-1], 64, W=Normal(0.05), nonlinearity=nn.lrelu, name='dz2')))
+discz_layers.append(ll.DropoutLayer(discz_layers[-1], p=0.5))
+
+# for x
+disc_layers = [x_input]
 disc_layers.append(ll.DropoutLayer(disc_layers[-1], p=0.2))
-disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 128, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu)))
-disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 128, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu)))
-disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 128, (3,3), pad=1, stride=2, W=Normal(0.05), nonlinearity=nn.lrelu)))
+disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 96, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu, name='dcnn1')))
+disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 96, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu, name='dcnn2')))
+disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 96, (3,3), pad=1, stride=2, W=Normal(0.05), nonlinearity=nn.lrelu, name='dcnn3')))
 disc_layers.append(ll.DropoutLayer(disc_layers[-1], p=0.5))
-disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 256, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu)))
-disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 256, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu)))
-disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 256, (3,3), pad=1, stride=2, W=Normal(0.05), nonlinearity=nn.lrelu)))
+disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 192, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu, name='dcnn4')))
+disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 192, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu, name='dcnn5')))
+disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 192, (3,3), pad=1, stride=2, W=Normal(0.05), nonlinearity=nn.lrelu, name='dcnn6')))
 disc_layers.append(ll.DropoutLayer(disc_layers[-1], p=0.5))
-disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 512, (3,3), pad=0, W=Normal(0.05), nonlinearity=nn.lrelu)))
-disc_layers.append(nn.weight_norm(ll.NINLayer(disc_layers[-1], num_units=256, W=Normal(0.05), nonlinearity=nn.lrelu)))
-disc_layers.append(nn.weight_norm(ll.NINLayer(disc_layers[-1], num_units=128, W=Normal(0.05), nonlinearity=nn.lrelu)))
+disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 192, (3,3), pad=0, W=Normal(0.05), nonlinearity=nn.lrelu, name='dcnn7')))
+disc_layers.append(nn.weight_norm(ll.NINLayer(disc_layers[-1], num_units=192, W=Normal(0.05), nonlinearity=nn.lrelu, name='dnin1')))
+disc_layers.append(nn.weight_norm(ll.NINLayer(disc_layers[-1], num_units=192, W=Normal(0.05), nonlinearity=nn.lrelu, name='dnin2')))
 disc_layers.append(ll.GlobalPoolLayer(disc_layers[-1]))
+
+# combine with disc_z
+disc_layers.append(ll.ConcatLayer([disc_layers[-1], discz_layers[-1]]))
+
+# finalize
 disc_layers.append(nn.weight_norm(ll.DenseLayer(disc_layers[-1], num_units=10, W=Normal(0.05), nonlinearity=None), train_g=True, init_stdv=0.1))
 disc_params = ll.get_all_params(disc_layers, trainable=True)
 
@@ -87,19 +118,25 @@ labels = T.ivector()
 x_lab = T.tensor4()
 x_unl = T.tensor4()
 temp = ll.get_output(gen_layers[-1], deterministic=False, init=True)
-temp = ll.get_output(disc_layers[-1], x_lab, deterministic=False, init=True)
-init_updates = [u for l in gen_layers+disc_layers for u in getattr(l,'init_updates',[])]
+temp = ll.get_output(genz_layers[-1], {x_input: x_lab}, deterministic=False, init=True)
+temp = ll.get_output(disc_layers[-1], {x_input: x_lab}, deterministic=False, init=True)
+init_updates = [u for l in gen_layers+disc_layers+discz_layers+genz_layers for u in getattr(l,'init_updates',[])]
 
-output_before_softmax_lab = ll.get_output(disc_layers[-1], x_lab, deterministic=False)
-output_before_softmax_unl = ll.get_output(disc_layers[-1], x_unl, deterministic=False)
-output_before_softmax_gen = ll.get_output(disc_layers[-1], gen_dat, deterministic=False)
+genz_lab = ll.get_output(genz_layers[-1], {x_input: x_lab})
+genz_unl = ll.get_output(genz_layers[-1], {x_input: x_unl})
+
+output_before_softmax_lab = ll.get_output(disc_layers[-1], {x_input: x_lab, z_input: genz_lab}, deterministic=False)
+output_before_softmax_unl = ll.get_output(disc_layers[-1], {x_input: x_unl, z_input: genz_unl}, deterministic=False)
+output_before_softmax_gen = ll.get_output(disc_layers[-1], {x_input: gen_dat}, deterministic=False)
 
 # copies for stability regularizer
 unsup_weight_var = T.scalar('unsup_weight')
 x_lab2 = T.tensor4()
 x_unl2 = T.tensor4()
-output_before_softmax_lab2 = ll.get_output(disc_layers[-1], x_lab2, deterministic=False)
-output_before_softmax_unl2 = ll.get_output(disc_layers[-1], x_unl2, deterministic=False)
+genz_lab2 = ll.get_output(genz_layers[-1], {x_input: x_lab2})
+genz_unl2 = ll.get_output(genz_layers[-1], {x_input: x_unl2})
+output_before_softmax_lab2 = ll.get_output(disc_layers[-1], {x_input: x_lab2, z_input: genz_lab2}, deterministic=False)
+output_before_softmax_unl2 = ll.get_output(disc_layers[-1], {x_input: x_unl2, z_input: genz_unl2}, deterministic=False)
 
 
 l_lab = output_before_softmax_lab[T.arange(args.batch_size),labels]
@@ -111,12 +148,12 @@ loss_unl = -0.5*T.mean(l_unl) + 0.5*T.mean(T.nnet.softplus(l_unl)) + 0.5*T.mean(
 train_err = T.mean(T.neq(T.argmax(output_before_softmax_lab,axis=1),labels))
 
 # test error
-output_before_softmax = ll.get_output(disc_layers[-1], x_lab, deterministic=True)
+output_before_softmax = ll.get_output(disc_layers[-1], {x_input: x_lab, z_input: genz_lab}, deterministic=True)
 test_err = T.mean(T.neq(T.argmax(output_before_softmax,axis=1),labels))
 
 # Theano functions for training the disc net
 lr = T.scalar()
-disc_params = ll.get_all_params(disc_layers, trainable=True)
+disc_params = ll.get_all_params(disc_layers+discz_layers, trainable=True)
 
 # create disc loss
 loss_stab_lab = T.mean(lasagne.objectives.squared_error(
@@ -137,13 +174,13 @@ test_batch = th.function(inputs=[x_lab], outputs=output_before_softmax, givens=d
 samplefun = th.function(inputs=[],outputs=gen_dat)
 
 # Theano functions for training the gen net
-output_unl = ll.get_output(disc_layers[-2], x_unl, deterministic=False)
-output_gen = ll.get_output(disc_layers[-2], gen_dat, deterministic=False)
+output_unl = ll.get_output(disc_layers[-2], {x_input: x_unl, z_input: genz_unl}, deterministic=False)
+output_gen = ll.get_output(disc_layers[-2], {x_input: gen_dat}, deterministic=False)
 m1 = T.mean(output_unl,axis=0)
 m2 = T.mean(output_gen,axis=0)
 loss_gen = T.mean(abs(m1-m2))
 
-gen_params = ll.get_all_params(gen_layers, trainable=True)
+gen_params = ll.get_all_params(gen_layers + genz_layers, trainable=True)
 gen_param_updates = nn.adam_updates(gen_params, loss_gen, lr=lr, mom1=0.5)
 train_batch_gen = th.function(inputs=[x_unl,lr], outputs=None, updates=gen_param_updates)
 
@@ -182,7 +219,7 @@ for epoch in range(900):
     begin = time.time()
 
     # set unsupervised weight
-    scaled_unsup_weight_max = 100
+    scaled_unsup_weight_max = 100. * args.count * 10. / 52000.
     rampup_value = rampup(epoch)
     unsup_weight = rampup_value * scaled_unsup_weight_max
     unsup_weight = np.cast[th.config.floatX](unsup_weight)
@@ -221,7 +258,7 @@ for epoch in range(900):
     
     if epoch==0:
         print(trainx.shape)
-        init_param(trainx[:500]) # data based initialization
+        init_param(trainx[:100]) # data based initialization
 
     # train
     loss_lab = 0.
@@ -254,7 +291,7 @@ for epoch in range(900):
         test_pred[first_ind:last_ind] = test_batch(testx[first_ind:last_ind])
     test_err = np.mean(np.argmax(test_pred,axis=1) != testy)
 
-    expname = 'cifar10-ali-pimodel-%.4fuw-annealing-noaugment-seed%d' % (args.unlabeled_weight, args.seed)
+    expname = 'cifar10-ali-pimodel-%.4fuw-corrected-noaugment-seed%d' % (args.unlabeled_weight, args.seed)
     out_str = "Experiment %s, Iteration %d, time = %ds, loss_lab = %.4f, loss_unl = %.4f, train err = %.4f, test err = %.4f" % (expname, epoch, time.time()-begin, loss_lab, loss_unl, train_err, test_err)
     print(out_str)
     sys.stdout.flush()
